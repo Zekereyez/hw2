cf972642ae8ff7d1c4093c49e53b548c436774b6
Ezequiel Reyes <reyeseze@usc.edu>
2022-09-29 23:15:11 +0000

    commit for format and tidy

---
+++ datastore.cpp
@@ -0,0 +1,19 @@
+#include "datastore.h"
+
+// will include the core functionality of searching
+// adding products, and users. As well as saving into 
+// the database.
+
+// Search: use the set intersection and setunion functions
+// inside of util.h 
+
+// look at the header file 
+
+// for cart, product, and users use maps 
+// want to associate username to user
+// keywords to products
+
+// keep map in data store as a member data structure
+
+// do the same for user and etc 
+
